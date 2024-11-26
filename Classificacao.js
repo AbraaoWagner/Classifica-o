5a268b615251da697f95833029a923d400102b92
AbraaoWagner <abraogamer60@gmail.com>
2024-11-26 02:34:11 -0300

    second commit

--- Classificacao.js
+++ Classificacao.js
@@ -1,1 +1,21 @@
-console.log("primeiro commit")
+class Hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+    atacar(){
+        const ataques = {
+            mago: "magia",
+            guerreiro: "espada",
+            monge: "artes maciais",
+            ninha: "shuriken"
+        }
+        console.log(`O ${this.tipo} atacou usando ${ataques[this.tipo]}!`)
+    }
+}
+
+let newHero = new Hero("Karenyou", 45, "mago")
+console.log(newHero)
+
+newHero.atacar()
